8f20fb522164f8c27b10bb77029c608ee9db5bd0
Wilson Cortez <wilsoncortez45@hotmail.com>
2023-10-17 14:16:46 +0100

    verificando a prop que estou a resceber

--- src/app/components/Notes/notes.tsx
+++ src/app/components/Notes/notes.tsx
@@ -1,9 +1,21 @@
+import React from 'react';
 
-
-export default function Notes(){
+export default function Notes(props: any){
+    const {data} = props;
+    let msg: string;
+    
+    if(data === 1 || data  <= 4){
+       return msg = 'Reprovado'
+    }else if(data === 5 || data  <= 9){
+        return msg = 'Recurso'
+    }else if(data === 9){
+        return msg = 'Aprovado'
+    }else {
+        msg = 'Nota inválida';
+      }
     return(
         <>
-            Carregando Notes...
+            Carregando Notes...{msg}
         </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
 import Notes from './components/Notes/notes'
 
 export default function Home() {
+  let data: number = 6
   return (
   <>
     <h3>Iniciando</h3>
-    <Notes />
+    <Notes data={data} />
   </>
   )
 }
