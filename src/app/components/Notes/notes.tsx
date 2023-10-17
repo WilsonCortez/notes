ba85d0a5b49d6dd63b1cf940fe3ca80c6ea5a251
Wilson Cortez <wilsoncortez45@hotmail.com>
2023-10-17 10:11:01 +0100

    Carregando o componente notes no componente principal

---
+++ src/app/components/Notes/notes.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Notes(){
+    return(
+        <>
+            Carregando Notes...
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Notes from './components/Notes/notes'
 
 export default function Home() {
   return (
   <>
     <h3>Iniciando</h3>
+    <Notes />
   </>
   )
 }
